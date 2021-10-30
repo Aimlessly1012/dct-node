a1d8131e4e0a3c2e0c0e094c8f2207a40a650864
Aimlessly1012 <8389724+sane_interspace@user.noreply.gitee.com>
2021-10-30 17:17:16 +0800

    first commit1

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -79,41 +79,14 @@ export default class HomeController extends Controller {
     //   Rtc Examples
     const appID = "91087211e39d4868a9f791fde2ea1d27";
     const appCertificate = "e4182d177dbc44eea21315c8601ad77c";
-    const channelName = "impeco";
-    //   const uid = 2882341273;
-    //   const account = "2882341273";
-    // const role = RtcRole.PUBLISHER;
-
-    const expirationTimeInSeconds = 3600;
-
-    const currentTimestamp = Math.floor(Date.now() / 1000);
-
-    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-
-    //   // IMPORTANT! Build token with either the uid or with the user account. Comment out the option you do not want to use below.
-
-    //   // Build token with uid
+    const channelName = "peco";
+    const uid = "0";
     const tokenA = RtcTokenBuilder.buildTokenWithUid(
       appID,
       appCertificate,
       channelName,
-      privilegeExpiredTs,
-      // role
-      // uid,
+      uid
     );
-    console.log("Token With Integer Number Uid: " + tokenA);
-
-    //   // Build token with user account
-    // const tokenB = RtcTokenBuilder.buildTokenWithAccount(
-    //   appID,
-    //   appCertificate,
-    //   channelName,
-    //   // account,
-    //   // role,
-    //   privilegeExpiredTs,
-    // );
-    // console.log("Token With UserAccount: " + tokenB);
-
     const { ctx } = this;
     mockTokenType.data = tokenA;
     ctx.body = mockTokenType;
